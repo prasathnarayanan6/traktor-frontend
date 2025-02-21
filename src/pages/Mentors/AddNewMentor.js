eb210a0debe341266caba8d3eb96d1075a5c458b
prasathnarayanan6 <prasathnarayanan6@gmail.com>
2025-02-21 14:28:49 +0530

    done

--- src/pages/Mentors/AddNewMentor.js
+++ src/pages/Mentors/AddNewMentor.js
@@ -12,40 +12,22 @@ import { useNavigate } from "react-router-dom";
 import { ApiAddNewMentor } from "../../API/API";
 import backarrow from '../../assets/images/Frame (1).svg';
 import messagesvg from '../../assets/images/Frame (2).svg';
+import messagesvgblack from '../../assets/images/Frame (22).svg';
 import settingsvg from '../../assets/images/Frame (3).svg';
+import professionalsvgblack from '../../assets/images/Frame (24).svg';
 import professionalsvg from '../../assets/images/Frame (4).svg';
+import settingsvgblack from '../../assets/images/Frame (25).svg';
+import exclamtionwhite from '../../assets/images/Frame (26).svg';
+import settingsvgwhite from '../../assets/images/Frame (27).svg';
 // import toast from "react-hot-toast/headless";
 import toast from 'react-hot-toast';
 function AddNewMentor() {
     const [currentStep, setCurrentStep] = useState(0);
     const navigate = useNavigate();
-    const steps = [
-        'Description',
-        'Professional',
-        'Contact',
-    ];
-    console.log(steps.length);
-    const icons = [
-        <FaMessage size={20} />,
-        <FaInfo size={20} />,
-        <FaGear size={20} />
-    ];
-
-    const nextPrev = (n) => {
-        setCurrentStep((prevStep) => prevStep + n);
-    };
-
-    const nxtstep = () => {
-        setCurrentStep(() => {
-            if(steps.length <= 3){
-                return currentStep + 1;
-            }
-        })
-    }
-
     const [formData, setFormData] = useState({
         description: {
             mentor_name: '',
+            choose_logo: '',
             mentor_description: ''
         },
         professional: {
@@ -75,48 +57,20 @@ function AddNewMentor() {
             },
         }));
     };
-
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        try {
-            const result = await ApiAddNewMentor(formData);
-            if(result)
-            {
-                toast.success("Mentor Created");
-                navigate('/addmentor');
-            }
-        } catch (err) {
-            if(err.response)
-            {
-                console.log(err.response.status);
-                    
-                    if(err.response.status==400)
-                    {
-                        toast.error('Please fill necessary data')
-                    }
-                    else if(err.response.status==401)
-                    {
-                        toast.error("Please Provide Valid Email");
-                    }
-                    else if(err.response.status==402)
-                    {
-                        toast.error('Phone number is not valid')
-                    }
-                    else if(err.response.status==409)
-                    {
-                        toast.error('Already exists')
-                    }
-            }
-            else {
-                console.log(err);
-            }
-                  
-        }
-    };
     const [showw, setShoww] = useState(false);
     useEffect(() => {
         setShoww(true);
     }, [])
+    const [nextt, setNextt] = useState(0)
+    const handleNext = (e) => {
+          e.preventDefault();
+          setNextt(nextt + 1);
+    }
+    let handleBack = (e) => {
+        e.preventDefault();
+        setNextt(nextt - 1);
+    }
+    console.log(formData);
     return (
         <div className="flex">
             <div>
@@ -132,38 +86,29 @@ function AddNewMentor() {
                                         <span><img src={backarrow} className="w-[90%]"/></span>
                                         <span className="">Add New Mentor</span>
                                     </div>
-                                    <div className="grid grid-cols-3 gap-5 mt-2">
-                                            <div className="flex justify-center items-center border bg-[#45C74D] p-3 rounded-xl">
-                                                    <div><img src={messagesvg} className="w-[80%]"/></div>
-                                                    <div className="text-white text-lg">Description</div>
+                                    <div className="grid grid-cols-3 mt-2">
+                                            <div className={`flex justify-center items-center border bg-[#45C74D] p-3 py-5 ${nextt==0 && 'bg-[#45C74D]' || 'bg-[#D8F3D9]'}`} style={{clipPath: "polygon(0% 0%, 90% 0%, 100% 50%, 90% 100%, 0% 100%)",}}>
+                                                    <div><img src={`${nextt==0 && messagesvg || messagesvgblack}`} className="w-[80%]"/></div>
+                                                    <div className={`text-black text-lg ${nextt == 0 && 'text-white'}`}>Description</div>
                                             </div> 
-                                            <div className="flex justify-center items-center border bg-[#D8F3D9] p-3 rounded-xl">
-                                                    <div><img src={professionalsvg} className="w-[80%]"/></div>
-                                                    <div className="text-lg">Professional</div>
-                                            </div>  
-                                            <div className="flex justify-center items-center border bg-[#D8F3D9] p-3 rounded-xl">
-                                                    <div className="text-white"><img src={settingsvg} className="w-[80%]"/></div>
-                                                    <div className="text-lg">Contact</div>
+                                            <div className={`flex justify-center items-center border bg-[#45C74D] p-3 py-5 ${nextt==1 && 'bg-[#45C74D]' || 'bg-[#D8F3D9]'}`} style={{clipPath: "polygon(89% 0%, 100% 50%, 89% 100%, 0% 100%, 9% 50%, 0% 0%)",}}>
+                                                <div><img src={`${nextt==1 && exclamtionwhite || professionalsvgblack}`} className="w-[80%]"/></div>
+                                                <div className={`text-black text-lg ${nextt == 1 && 'text-white'}`}>Professional</div>
+                                            </div>
+                                            <div className={`flex justify-center items-center border bg-[#45C74D] p-3 py-5 ${nextt==2 && 'bg-[#45C74D]' || 'bg-[#D8F3D9]'}`} style={{clipPath: "polygon(100% 0%, 100% 49%, 100% 100%, 0% 100%, 9% 50%, 0% 0%)",}}>
+                                                    <div><img src={`${nextt==2 && settingsvgwhite || settingsvg}`} className="w-[80%]"/></div>
+                                                    <div className={`text-lg text-lg ${nextt == 2 && 'text-white'}`}>Contact</div>
                                             </div>  
                                     </div>
                                     <div className="mt-7">
-                                        <div className="grid grid-cols-2 gap-5">
-                                                    <div>
-                                                        <label className="text-md">Name of the mentor</label>
-                                                        <input type="text" className="w-[100%] block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-[#45C74D] focus:border-[#45C74D] mt-1"/>
-                                                    </div>
-                                                    <div>
-                                                            <label>Choose logo</label>
-                                                            <input type="text" className="w-[100%] block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-[#45C74D] focus:border-[#45C74D] mt-1"/>
-                                                    </div>
-                                                    
-                                        </div>
-                                        <div className="mt-2">
-                                                            <label>Description</label>
-                                                            <textarea className="w-[100%] block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-[#45C74D] focus:border-[#45C74D] mt-1 resize-none "/>
-                                        </div>
-                                        <div className="flex justify-center item-center mt-7">
-                                            <button className="bg-[#45C74D] p-2 text-white rounded-lg" onClick={nxtstep}>Next</button>
+                                        {nextt == 0 && <Step1 formData={formData.description} handleChange={(e) => handleChange(e, 'description')}/>}
+                                        {nextt == 1 && <Step2 formData={formData.professional} handleChange={(e) => handleChange(e, 'professional')}/>}
+                                        {nextt == 2 && <Step3 formData={formData.contact} handleChange={(e) => handleChange(e, 'contact')}/>}
+                                        <div className="flex justify-center item-center mt-7 gap-4">
+                                            {nextt == 1 && <button className={`bg-[#45C74D] p-2 text-white rounded-lg`} onClick={handleBack}>Back</button>}
+                                            {nextt == 2 && <button className={`bg-[#45C74D] p-2 text-white rounded-lg`} onClick={handleBack}>Back</button>}
+                                            <button className={`bg-[#45C74D] p-2 text-white rounded-lg ${nextt == 2 && 'hidden'}`} onClick={handleNext}>Next</button>
+                                            {nextt == 2 && <button className={`bg-[#45C74D] p-2 text-white rounded-lg`}>Submit</button>}   
                                         </div>
                                     </div>
                             </div>
--- src/pages/Mentors/Step/Step1.js
+++ src/pages/Mentors/Step/Step1.js
@@ -2,32 +2,23 @@ import React from "react";
 
 const Step1 = ({formData, handleChange}) => {
   return (
-    <div className="grid grid-cols-1 gap-4 items-center px-10 mb-5">
-      <div className="relative">
-        <input
-          type="text"
-          id="floating_outlined"
-          name="mentor_name"
-          onChange={handleChange}
-          value={formData.mentor_name}
-          className="block px-2.5 pb-2.5 pt-4 w-full  md:h-[45px] text-sm text-gray-900 bg-transparent rounded-lg border border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-          placeholder=" "
-        />
-        <label
-          htmlFor="floating_outlined"  // Changed 'for' to 'htmlFor'
-          className="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
-        >
-          Name of the mentor
-        </label>
-      </div>
-      <textarea
-            type="text"
-            name="mentor_description"  // Consistent naming
-            value={formData.description}  // Accessing 'description' correctly
-            onChange={handleChange}
-            className="block w-full mb-4 text-sm text-gray-900 bg-transparent rounded-lg border border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer resize-none"
-            placeholder="Description"
-      ></textarea>
+    <div>
+            <div className="grid grid-cols-2 gap-4 items-center mb-5">
+                  <div className="">
+                          <div className="">Name of the Mentor <span className="text-[#E54545]">*</span></div>
+                          <div className="mt-1"><input type="text" name="mentor_name" value={formData.mentor_name} onChange={handleChange} className="block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-[#45C74D] focus:border-[#45C74D]" placeholder="Enter name of the Mentor"/></div>        
+                  </div>
+                  <div className="">
+                          <div className="">Choose Logo <span className="text-[#E54545]">*</span></div>
+                          <div className="mt-1"><input type="text"  name="choose_logo" value={formData.choose_logo} onChange={handleChange} className="block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-[#45C74D] focus:border-[#45C74D]"/></div>        
+                  </div>
+            </div>
+            <div>
+                <div className="">
+                          <div className="">Description <span className="text-[#E54545]">*</span></div>
+                          <div className="mt-1"><textarea type="text" name="mentor_description" value={formData.mentor_description} onChange={handleChange} className="block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-[#45C74D] focus:border-[#45C74D] resize-none" placeholder="Type here..."/></div>        
+                  </div>
+            </div>
     </div>
   );
 };
